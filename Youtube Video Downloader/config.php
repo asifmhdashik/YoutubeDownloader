97d5b04068821cbc74cc6a0dd0f8051aa04cb015
asifmhdashik <76432849+asifmhdashik@users.noreply.github.com>
2021-02-04 21:54:07 +0530

    Add files via upload

---
+++ Youtube Video Downloader/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array();
+
+$config['site_title'] = ' YouTube Video Downloader '; // Website Title
+
+$config['site_meta'] = 'The Best Free  YouTube Video  Audio Downloading Tool'; // Site meta description
+
+$config['site_url'] = 'http://catonite.website/mokoviralyoutube/'; // Site url with slash
+
+$config['enable_ads'] = true; // Enable ads? true / false
+
+$config['server_email'] = 'asifmashik@gmail.com'; // Your server email address where all emails will be sent FROM
+
+$config['contact_email'] = 'asifmashik@gmail.com'; // Your personal email to receive all contact email
+
+$config['ads_code'] = '<div class="ads"><img src="img/ads.png" alt="ads banner"></div>' //paste your ad code into the quotemarks
+
+?>
